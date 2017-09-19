53b3b914f06aeaaead7c785249246177f9082310
Jarppe <jarppe@metosin.fi>
2017-09-19 19:51:16 +0300

    Replaced byte code gen proxy with regular proxy of OutputStream.

--- eftest/src/eftest/output_capture.clj
+++ eftest/src/eftest/output_capture.clj
@@ -1,7 +1,6 @@
 (ns eftest.output-capture
-  (:import (java.io PrintStream PrintWriter ByteArrayOutputStream)
-           (java.nio.charset StandardCharsets)
-           (javassist.util.proxy ProxyFactory MethodFilter MethodHandler)))
+  (:import (java.io OutputStream ByteArrayOutputStream PrintStream PrintWriter)
+           (java.nio.charset StandardCharsets)))
 
 (def captured-output-contexts (atom {}))
 
@@ -10,49 +9,36 @@
       (.getId)))
 
 (defn begin-capture []
-  (let [buffer (ByteArrayOutputStream.)
-        context {:buffer buffer
-                 :stream (PrintStream. buffer)
-                 :writer (PrintWriter. buffer)}]
-    (swap! captured-output-contexts assoc (thread-id) context)
-    context))
+  (let [buffer (ByteArrayOutputStream.)]
+    (swap! captured-output-contexts assoc (thread-id) buffer)
+    buffer))
 
-(defn get-capture-context []
+(defn get-capture-buffer ^ByteArrayOutputStream []
   (get @captured-output-contexts (thread-id)))
 
 (defn flush-captured-output ^String []
-  (let [context (get-capture-context)
-        {:keys [^ByteArrayOutputStream buffer ^PrintStream stream ^PrintWriter writer]} context]
-    (when buffer
-      (.flush stream)
-      (.flush writer)
-      (-> buffer .toByteArray (String. StandardCharsets/UTF_8)))))
+  (some-> (get-capture-buffer)
+          .toByteArray
+          (String. StandardCharsets/UTF_8)))
 
-(defn create-proxy [superclass context-key]
-  (let [factory (doto (ProxyFactory.)
-                  (.setSuperclass superclass)
-                  (.setFilter (reify MethodFilter
-                                (isHandled [this m] true))))
-        handler (reify MethodHandler
-                  (invoke [this self method proceed args]
-                    (let [target (get (get-capture-context) context-key)]
-                      (.invoke method target args))))]
-    (.create factory
-             (into-array Class [java.io.OutputStream])
-             (into-array Object [(ByteArrayOutputStream.)])
-             handler)))
-
-(defn create-proxy-print-stream ^PrintStream []
-  (create-proxy java.io.PrintStream :stream))
-
-(defn create-proxy-print-writer ^PrintWriter []
-  (create-proxy java.io.PrintWriter :writer))
+(defn create-proxy-output-stream ^OutputStream []
+  (proxy [OutputStream] []
+    (write
+      ([data]
+       (let [target (get-capture-buffer)]
+         (if (instance? Integer data)
+           (.write target ^int data)
+           (.write target ^bytes data 0 (alength ^bytes data)))))
+      ([data off len]
+       (let [target (get-capture-buffer)]
+         (.write target data off len))))))
 
 (defn init-capture []
   (let [old-out System/out
         old-err System/err
-        new-stream (create-proxy-print-stream)
-        new-writer (create-proxy-print-writer)]
+        proxy-output-stream (create-proxy-output-stream)
+        new-stream (PrintStream. proxy-output-stream)
+        new-writer (PrintWriter. proxy-output-stream)]
     (System/setOut new-stream)
     (System/setErr new-stream)
     {:captured-writer new-writer
