6311872340bc951364670177f56adb76122143b8
Jarppe <jarppe@metosin.fi>
2017-09-19 18:50:11 +0300

    Revert "Set version to 0.3.2-SNAPSHOT."
    This reverts commit bc2834b9ae089ca53a6d044385b87b76f193c0fe.

--- lein-eftest/src/leiningen/eftest.clj
+++ lein-eftest/src/leiningen/eftest.clj
@@ -5,7 +5,7 @@
             [leiningen.core.project :as project]))
 
 (def eftest-profile
-  {:dependencies '[[eftest "0.3.2-SNAPSHOT"]]})
+  {:dependencies '[[eftest "0.3.1"]]})
 
 (defn- report-namespace [project]
   (-> project :eftest (:report 'eftest.report.progress/report) namespace symbol))
